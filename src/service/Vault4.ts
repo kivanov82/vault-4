55919450e229b194dec22a95be9d8fdd0a7ebcfc
Kiril Ivanov <kivanov82@yahoo.com>
2026-01-11 22:08:04 +0100

    Minor fixes prior to the deployment

--- src/service/Vault4.ts
+++ src/service/Vault4.ts
@@ -7,8 +7,8 @@ export class Vault4 {
     static async init(): Promise<any> {
         logger.info("Vault service initializing");
         try {
-            const plan = await DepositService.buildDepositPlan({ refreshCandidates: true });
-            await DepositService.executeDepositPlan(plan, {dryRun: false, minDepositUsd: 1});
+            //const plan = await DepositService.buildDepositPlan({ refreshCandidates: true });
+            //await DepositService.executeDepositPlan(plan, {dryRun: false, minDepositUsd: 1});
         } catch (error: any) {
             logger.warn("Failed to build deposit plan during startup", {
                 message: error?.message,
